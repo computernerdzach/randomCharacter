0d3463b97e5e46cf64ec971b0569cd4455d40150
computernerdzach <wizteczach@gmail.com>
2020-10-20 14:16:13 -0700

    added help function and random background/feature function

--- randomCharacter.py
+++ randomCharacter.py
@@ -20,15 +20,10 @@ def race_class():
                 f'{random.choice(alignments)}.```'
     return character
 
+
 def stat_roll():
-    stat1 = []
-    stat2 = []
-    stat3 = []
-    stat4 = []
-    stat5 = []
-    stat6 = []
-    stat7 = []
-    stats = [stat1, stat2, stat3, stat4, stat5, stat6, stat7]
+    stats = [[], [], [], [], [], [], []]
+    newList = []
     for each in stats:
         each.append(random.randint(1, 6))
         each.append(random.randint(1, 6))
@@ -36,12 +31,28 @@ def stat_roll():
         each.append(random.randint(1, 6))
     for each in stats:
         each.remove(min(each))
-    newList = []
-    for each in stats:
         newList.append(sum(each))
     newList.remove(min(newList))
-    string = f'```Your stats are {newList[0]} {newList[1]} {newList[2]} {newList[3]} {newList[4]} {newList[5]}```'
-    return string
+    return f'```Your stats are {newList[0]} {newList[1]} {newList[2]} {newList[3]} {newList[4]} {newList[5]}.```'
+
+
+def help_message():
+    return '''```Welcome to randomCharacter
+enter "!dnd" to get the attention of the bot.
+in the same message include your command:
+    "character" generates a character for you.
+    "stats" gives you an array of stats. 4d6 are rolled seven times. The lowest die is dropped from each group, and the lowest group is dropped. This tends to build more powerful characters.
+    "background" generates a background and feature for you.
+    "help" shows you this message.```'''
+
+
+def background():
+    backgrounds = {'acolyte': 'shelter of the faithful', 'criminal/spy': 'criminal contact',
+                   'folk hero': 'rustic hospitality', 'noble': 'position of privilege', 'sage': 'researcher',
+                   'soldier': 'military rank'}
+    bg = random.choice(list(backgrounds))
+    feature = backgrounds[bg]
+    return f'```Your background is {bg}, which grants you {feature}.```'
 
 
 @client.event
@@ -60,5 +71,9 @@ async def on_message(message):
             await message.channel.send(race_class())
         if 'stats' in message.content.lower():
             await message.channel.send(stat_roll())
+        if 'background' in message.content.lower():
+            await message.channel.send(background())
+        if 'help' in message.content.lower():
+            await message.channel.send(help_message())
 
 client.run(TOKEN)
